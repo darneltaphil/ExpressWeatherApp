89802467ef20e88f1c147755d93077425d170c1c
darneltaphil <darneltaphil@gmail.com>
2020-09-30 12:09:42 +0000

    Static Files served in client/build

--- app.js
+++ app.js
@@ -10,19 +10,12 @@ const app = express();
 // });
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, 'react-weather-app/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //production mode
  if(process.env.NODE_ENV === 'production') {  
-   app.use(express.static(path.join(__dirname, 'react-weather-app/build')));
+   app.use(express.static(path.join(__dirname, 'client/build')));
   }
- 
-  //app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'react-weather-app/build/index.html'));})
-
-
-//build mode
- //app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/react-weather-app/public/index.html'));});
-
 
 //routing to the weather display
 app.get('/getWeather/:town', (req,res) => {
