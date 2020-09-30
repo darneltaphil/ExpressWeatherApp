b2e1cf13d11c39fb99a29c174a35c44cfcbf4c21
darneltaphil <darneltaphil@gmail.com>
2020-09-30 08:34:40 +0000

    prepare to deploy

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const request = require('request');
 const path = require('path');
-
+const port = process.env.PORT || 5000;
 const app = express();
 
 // Root render
-app.get('/', (req,res) => {
-        res.send('nothing is here')
-});
+// app.get('/', (req,res) => {
+//         res.send('nothing is here')
+// });
 
-//app.set('views', path.join(__dirname, 'build'));
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'react-weather-app/build')));
+
+//production mode
+ if(process.env.NODE_ENV === 'production') {  
+   app.use(express.static(path.join(__dirname, 'react-weather-app/build')));
+  }
+ 
+  //app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'react-weather-app/build/index.html'));})
+
+
+//build mode
+ //app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/react-weather-app/public/index.html'));});
+
 
 //routing to the weather display
-app.get('/getweather/:town', (req,res) => {
+app.get('/getWeather/:town', (req,res) => {
   const location = req.params.town;
   request('http://api.weatherstack.com/current?access_key=837e00581a14b54b44fcb5f46b8d591b&query='+location, 
   (err,req, body) => {
@@ -22,15 +35,16 @@ app.get('/getweather/:town', (req,res) => {
 });
 
 // An api endpoint that returns the login page
-app.get('/login', (req,res) => {
-   console.log('I am going to work on that soon');
-});
+// app.get('/login', (req,res) => {
+//    console.log('I am going to work on that soon');
+// });
 
-app.get('*', (req,res) =>{
-  res.sendStatus(404);
-});
+// app.get('*', (req,res) =>{
+//   res.sendStatus(404);
+// });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
 
-console.log('App is listening on port ' + port);
+// app.listen(port);
+//start server
+app.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);})
+//console.log('App is listening on port ' + port);
