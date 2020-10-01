57ae0492971cd8b6835de86965a24cd2ee57407b
darneltaphil <darneltaphil@gmail.com>
2020-10-01 02:57:27 +0000

    Ready to deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const request = require('request');
-const path = require('path');
+//const path = require('path');
 const port = process.env.PORT || 5000;
 const app = express();
 
